60c89750a2c3215e36e62d10e33f80b098f7fba0
EmmanuelBazan <emmanuelbazan99@gmail.com>
2023-10-24 23:23:29 -0600

    Genre buttons style

--- app/src/main/java/com/stressMaker/myapplication/MenuActivity.kt
+++ app/src/main/java/com/stressMaker/myapplication/MenuActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.stressMaker.myapplication.imccalculator.ImcCalculatorActivity
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,12 +12,23 @@ class MenuActivity : AppCompatActivity() {
         setContentView(R.layout.activity_menu)
 
         val btnSaludaApp = findViewById<Button>(R.id.btnSaludaApp)
+        val btnImcApp = findViewById<Button>(R.id.btnIMCApp)
+
         btnSaludaApp.setOnClickListener() {
             navigateToSaludaApp()
         }
+
+        btnImcApp.setOnClickListener() {
+            navigateToImcApp()
+        }
     }
 
-    fun navigateToSaludaApp() {
+    private fun navigateToImcApp() {
+        val intent = Intent(this, ImcCalculatorActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun navigateToSaludaApp() {
         val intent = Intent(this, FirstAppActivity::class.java)
         startActivity(intent)
     }
