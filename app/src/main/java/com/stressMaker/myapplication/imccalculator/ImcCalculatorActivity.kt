b2c6a16d4d4053c62a8845ad4b2616c05b6b2cd3
EmmanuelBazan <emmanuelbazan99@gmail.com>
2023-11-01 23:44:16 -0600

    Weight and age controls

--- app/src/main/java/com/stressMaker/myapplication/imccalculator/ImcCalculatorActivity.kt
+++ app/src/main/java/com/stressMaker/myapplication/imccalculator/ImcCalculatorActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.slider.RangeSlider
 import com.stressMaker.myapplication.R
 import java.text.DecimalFormat
@@ -15,9 +16,18 @@ class ImcCalculatorActivity : AppCompatActivity() {
     private lateinit var viewFemale:CardView
     private lateinit var txtHeight:TextView
     private lateinit var rsHeight:RangeSlider
+    private lateinit var btnPlus:FloatingActionButton
+    private lateinit var btnMin:FloatingActionButton
+    private lateinit var txtWeight:TextView
+    private lateinit var txtAge:TextView
+    private lateinit var btnMinAge:FloatingActionButton
+    private lateinit var btnPlusAge:FloatingActionButton
 
     private var isMale:Boolean = true
     private  var isFemale:Boolean = false
+
+    private var weight:Int = 60;
+    private var age:Int = 28
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +35,7 @@ class ImcCalculatorActivity : AppCompatActivity() {
 
         initComponents()
         initListeners()
+        initUI()
     }
 
     private fun initComponents() {
@@ -32,15 +43,48 @@ class ImcCalculatorActivity : AppCompatActivity() {
         viewFemale = findViewById(R.id.cardFemale)
         txtHeight = findViewById(R.id.txtHeight)
         rsHeight = findViewById(R.id.rsHeight)
+        txtWeight = findViewById(R.id.txtWeight)
+        btnPlus = findViewById(R.id.btnPlus)
+        btnMin = findViewById(R.id.btnMin)
+        txtAge = findViewById(R.id.txtAge)
+        btnMinAge = findViewById(R.id.btnMinAge)
+        btnPlusAge = findViewById(R.id.btnPlusAge)
+    }
+
+    private fun initUI() {
+        setWeight()
+        setAge()
     }
 
     private fun initListeners() {
         viewMale.setOnClickListener { setGenderColor(false) }
+
         viewFemale.setOnClickListener { setGenderColor(true) }
+
         rsHeight.addOnChangeListener { _, value, _ ->
             val df = DecimalFormat("#.##")
             val res = df.format(value)
             txtHeight.text = "$res cm"
+        }
+
+        btnMin.setOnClickListener{
+            weight -= 1
+            setWeight()
+        }
+
+        btnPlus.setOnClickListener{
+            weight += 1
+            setWeight()
+        }
+
+        btnPlusAge.setOnClickListener {
+            age += 1
+            setAge()
+        }
+
+        btnMinAge.setOnClickListener{
+            age -= 1
+            setAge()
         }
     }
 
@@ -55,5 +99,13 @@ class ImcCalculatorActivity : AppCompatActivity() {
             viewFemale.setCardBackgroundColor(colorUnselected)
             viewMale.setCardBackgroundColor(colorSelected)
         }
+    }
+
+    private fun setWeight() {
+        txtWeight.text = "${weight.toString()} Kg"
+    }
+
+    private fun setAge() {
+        txtAge.text = age.toString()
     }
 }
